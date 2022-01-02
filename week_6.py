d3e50cc0232f61c8db470d7cc48b404aee3acc2f
chizoe <chizoehsieh@gmail.com>
2022-01-02 10:52:37 +0800

    'try'

--- week_6.py
+++ week_6.py
@@ -8,3 +8,4 @@ print(c[c.find('best'):])  # 尋找'best'的索引值後，顯示索引值後的
 print(c[c.find('NIU'):c.find('IE')])  # 尋找'N'、'E'的索引值，顯示 N~E 的所有元素
 print(c[c.find('ou'):c.find('t')+1:3])  # 尋找'o'、't'的索引值，從'o'到't'每三個字元顯示一次
 # 2022/1/2 修改
+print('hello 028')
